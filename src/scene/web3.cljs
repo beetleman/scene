a27cdc400dc47be692c57c05e30ba18bc958e11e
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-10-16 00:59:43 +0200

    subscribe

--- src/scene/web3.cljs
+++ src/scene/web3.cljs
@@ -4,8 +4,20 @@
 
 (def Web3 (js/require "web3"))
 
-(defstate httpProvider
-  :start (Web3.providers.HttpProvider. config/rpc-url))
+(def httpProvider (Web3.providers.HttpProvider. config/rpc-url))
+(def web3 (Web3. httpProvider))
 
-(defstate web3
-  :start (Web3. httpProvider))
+
+(defn start-log-watcher [web3]
+  (.subscribe web3.eth "logs"
+              (clj->js {:fromBlock 0})
+              console.log))
+
+
+(defn stop-log-watcher [log-watcher]
+  (.unsubscribe log-watcher))
+
+
+(defstate log-watcher
+  :start (start-log-watcher web3)
+  :stop (stop-log-watcher @log-watcher))
