cb224fff8cd1e27ecf028471a733a03aeb940a6c
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-10-17 01:43:04 +0200

    send metacoins to random accouns

--- development/fireEvents.js
+++ development/fireEvents.js
@@ -2,24 +2,26 @@ const MetaCoin = artifacts.require("./MetaCoin.sol");
 const interval = process.env.INTERVAL || 1000;
 
 const createAccountGenerator = () => {
-  let accounts = [...web3.eth.accounts];
-  accounts.reverse();
+  const from  = web3.eth.accounts[0];
+  const length = from.length;
+  const maxAddress = web3.toDecimal(
+    '0x' + 'f'.repeat(length - 2)
+  );
   return () => {
-    const from = accounts.pop();
-    const to = accounts.pop();
-    accounts = [
-      from,
-      ...accounts,
-      to
-    ];
+    const to = web3.toHex(
+      Math.random() * maxAddress
+    ).padEnd(
+      length, 0
+    );
     return { from, to };
   };
 };
 
+
 const accountGenerator = createAccountGenerator();
 
 const fireEvents = async (coin) => (setInterval(async () => {
-  const amount = 42;
+  const amount = Math.floor(Math.random() * 1000);
   const { from, to } = accountGenerator();
   console.log(`send ${amount} metacoin from ${from} to ${to}`);
   await coin.sendCoin(to, amount, { from });
