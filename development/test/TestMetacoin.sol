87e245e85601b4bfd77d8c01faa14a18d4037ebd
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-10-18 23:56:38 +0200

    more test coins

--- development/test/TestMetacoin.sol
+++ development/test/TestMetacoin.sol
@@ -9,17 +9,17 @@ contract TestMetacoin {
   function testInitialBalanceUsingDeployedContract() {
     MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
-    uint expected = 10000;
+    uint expected = 1000000000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 1000000000 MetaCoin initially");
   }
 
   function testInitialBalanceWithNewMetaCoin() {
     MetaCoin meta = new MetaCoin();
 
-    uint expected = 10000;
+    uint expected = 1000000000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 1000000000 MetaCoin initially");
   }
 
 }
