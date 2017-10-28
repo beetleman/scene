dffbccd96d6a58b911782ef975b27ed330b39474
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-10-28 20:13:48 +0200

    `create-log-getter` now can be backpresured by `log-ch`

--- src/scene/web3/log.cljs
+++ src/scene/web3/log.cljs
@@ -43,7 +43,28 @@
   "create log getter geting block givent in `ranges-ch` chan
   and put them on `logs-ch` chan"
   [web3 ranges-ch logs-ch]
-  (go-loop [range (<! ranges-ch)]
+  (go-loop [range (<! ranges-ch)
+            result-ch (chan)]
     (-> (.filter (.. web3 -eth) (clj->js range))
-        (.get (utils/callback-chan-seq-fn logs-ch)))
-    (recur (<! ranges-ch))))
+        (.get (utils/callback-chan-fn result-ch)))
+    (doseq [log (:data (<! result-ch))]  ;TODO: error handlig[ignore all errors for now]
+      (>! logs-ch {:data log :erro nil}))
+    (recur (<! ranges-ch) result-ch)))
+
+
+;; TODO: REMOVE IT
+(def state (atom nil))
+(defn try-log-getter [web3]
+  (let [ranges (chan)
+        from 1000
+        to 2000
+        logs (chan 1000)
+        all (set (range from to))
+        get-number #(-> % :data :blockNumber)]
+    (a/onto-chan ranges
+                 (create-block-ranges from to 100)
+                 false)
+    (create-log-getter web3 ranges logs)
+    (go-loop [ seen #{}]
+      (reset! state (clojure.set/difference all seen))
+      (recur  (conj seen (get-number (<! logs)))))))
